cc9cd4b89ad7b6164e4dfcbfbd728130f8752248
YeonwooSung <neos960518@gmail.com>
2018-10-05 08:36:10 +0900

    WebView diplay success

--- App.js
+++ App.js
@@ -1,33 +1,11 @@
-import React, {Component} from 'react';
-import { 
-  StyleSheet,
-  View
-} from 'react-native';
-
+import React, { Component } from 'react';
 import Main from './components/Main';
 
 export default class App extends Component {
-  state = {
-    isLoaded: false
-  };
 
   render() {
-    const { isLoaded } = this.state;
     return (
-      <View style={styles.container}>
-        {
-          isLoaded ? <Main></Main> : <Main></Main>
-        }
-      </View>
+      <Main></Main>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#ffffff' //'#F5FCFF'
-  }
-});
